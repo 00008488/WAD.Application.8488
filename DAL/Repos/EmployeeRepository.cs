3f7631c22d0fbcf1309d704c1fc454a91804cc50
saidahror <saidahror@saidahror.com>
2021-06-19 01:12:32 +0500

    repos finished

---
+++ DAL/Repos/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repos
+{
+    public interface EmployeeRepository
+    {
+        public Employee GetById(String Id);
+        List<Employee> GetAll();
+        public bool Create(Employee e);
+        public bool Update(Employee e);
+        public bool Delete(String Id);
+    }
+}
---
+++ DAL/Repos/Impl/EmployeeRepositoryImpl.cs
@@ -0,0 +1,82 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL.Repos.Impl
+{
+    public class EmployeeRepositoryImpl : EmployeeRepository
+    {
+        private readonly DbContextConfig _ctx;
+
+        public EmployeeRepositoryImpl(DbContextConfig ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _ctx.Employees.ToList();
+        }
+
+        public Employee GetById(String Id)
+        {
+            return _ctx.Employees.Where(x => x.Id == Id).FirstOrDefault();
+        }
+
+        public bool Create(Employee d)
+        {
+            try
+            {
+                _ctx.Employees.Add(d);
+                _ctx.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+                return false;
+            }
+        }
+
+        public bool Update(Employee e)
+        {
+            var existing = GetAll().FirstOrDefault(x => x.Id == e.Id);
+            if (existing != null)
+            {
+                try
+                {
+                    existing.Name = e.Name;
+                    _ctx.SaveChanges();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+            return false;
+        }
+
+        public bool Delete(String Id)
+        {
+            try
+            {
+                _ctx.Employees.Remove(GetAll().First(x => x.Id == Id));
+                _ctx.SaveChanges();
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+            return false;
+        }
+
+    }
+}
