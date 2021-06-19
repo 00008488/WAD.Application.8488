3553340fcb910d433584928661525702b23b0a95
saidahror <saidahror@saidahror.com>
2021-06-19 16:34:32 +0500

    SPA: employees crud finished

--- WAD.Application.8488/wwwroot/js/angular/script.js
+++ WAD.Application.8488/wwwroot/js/angular/script.js
@@ -188,6 +188,11 @@ angular
                             if (success.data) {
                                 alert("Successfully deleted");
                                 window.location = "#!/employees";
+
+                                $http.get('/api/employee/getall').then(function (response) {
+                                    $scope.employees = response.data;
+                                });
+
                             } else {
                                 alert("Something happened")
                             }
@@ -195,6 +200,161 @@ angular
                             alert(error.status);
                         });
                 }
+            }
+        ]
+    )
+    .controller(
+        'EmployeesModifyController',
+        [
+            '$scope',
+            '$http',
+            '$location',
+            function ($scope, $http, $location) {
+
+                $scope.departments = [];
+                $http.get('/api/department/getall').then(function (response) {
+                    $scope.departments = response.data;
+                });
+
+                $scope.post_to = function (action_name_id, employee) {
+                    if (action_name_id == 'add') {
+
+                        let url = '/api/employee/create';
+
+                        if (employee) {
+
+                            if (!employee.Name) {
+                                alert("Empty Name Field")
+                                return;
+                            }
+                            if (!employee.Dob) {
+                                alert("Empty Dob Field")
+                                return;
+                            }
+                            if (!employee.DepartmentId) {
+                                alert("Empty Department Field")
+                                return;
+                            }
+
+                            let x = employee.Dob;
+                            let hoursDiff = x.getHours() - x.getTimezoneOffset() / 60;
+                            let minutesDiff = (x.getHours() - x.getTimezoneOffset()) % 60;
+                            x.setHours(hoursDiff);
+                            x.setMinutes(minutesDiff);
+
+                            employee.Dob = x;
+
+                            employee = {
+                                Name: employee.Name,
+                                Dob: employee.Dob,
+                                Department: null,
+                                DepartmentId: employee.DepartmentId
+                            }
+
+                            $http({
+                                method: 'POST',
+                                url: url,
+                                data: JSON.stringify(employee)
+                            })
+                                .then(function (success) {
+                                    if (success.data) {
+                                        alert("Successfully added");
+                                        window.location = "#!/employees";
+                                    } else {
+                                        alert("Something happened")
+                                    }
+                                }, function (error) {
+                                    alert(error.status);
+                                });
+                        } else {
+                            alert("Empty Employee form");
+                        }
+                    }
+
+                    if (action_name_id == 'edit') {
+
+                        let url = '/api/employee/update';
+                        if (employee) {
+
+                            if (!employee.Name) {
+                                alert("Empty Name Field")
+                                return;
+                            }
+                            if (!employee.Dob) {
+                                alert("Empty Dob Field")
+                                return;
+                            }
+                            if (!employee.DepartmentId) {
+                                alert("Empty Department Field")
+                                return;
+                            }
+
+                            let x = employee.Dob;
+                            let hoursDiff = x.getHours() - x.getTimezoneOffset() / 60;
+                            let minutesDiff = (x.getHours() - x.getTimezoneOffset()) % 60;
+                            x.setHours(hoursDiff);
+                            x.setMinutes(minutesDiff);
+
+                            employee.Dob = x;
+
+                            employee = {
+                                Name: employee.Name,
+                                Dob: employee.Dob,
+                                Department: null,
+                                DepartmentId: employee.DepartmentId,
+                                Id: $location.search().id
+                            }
+
+                            $http({
+                                method: 'POST',
+                                url: url,
+                                data: JSON.stringify(employee)
+                            })
+                                .then(function (success) {
+                                    if (success.data) {
+                                        alert("Successfully updated");
+                                        window.location = "#!/employees";
+                                    } else {
+                                        alert("Something happened")
+                                    }
+                                }, function (error) {
+                                    alert(error.status);
+                                });
+                        } else {
+                            alert("Empty Employee form");
+                        }
+                    }
+
+                }
+
+                $scope.$on('$viewContentLoaded', function () {
+                    let action = $location.search().action;
+                    let id = $location.search().id;
+
+                    if (action == '2') {
+                        $scope.action_name = "Edit";
+                        $scope.action_name_id = "edit";
+
+                        $scope.employeeId = id;
+                        $scope.employeeName = '';
+                        $scope.employeeDepartmentId = '';
+                        $scope.employeeDob = '';
+
+                        $http.get('/api/employee/get/' + id).then(function (response) {
+                            let emp = response.data;
+
+                            $scope.employeeName = emp.name;
+                            $scope.employeeDepartmentId = emp.department.id;
+                            $scope.employeeDob = emp.dob;
+                        })
+
+                    }
+                    else {
+                        $scope.action = "Add";
+                        $scope.action_name_id = "add";
+                    }
+                });
+
             }
         ]
     );
